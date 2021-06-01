b417bf03864ec2830b9a103efb003b4ecca9a8a3
kingZ1456 <78988802+kingZ1456@users.noreply.github.com>
2021-05-31 22:07:59 -0400

    First commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,9 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Menu from '../components/menu'
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -26,6 +20,8 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      
+      <Menu/>
       <div
         style={{
           margin: `0 auto`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,129 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1 style={{paddingLeft:'30px'}}>Hi, Nice to meet you, I'm <span style={{color:'blue', fontWeight:'bold'}}>Zohaib Adnan!</span></h1>
+
+    <div style={{display: 'flex',
+      }}>
+    
+    <div style={{paddingTop:'30px', paddingRight:'30px', paddingLeft:'30px'}}>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src="../images/image2.jpg"
       width={300}
+      height={350}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
       alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
+      style={{ marginBottom: `1.45rem`}}
     />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    </div>
+
+    <div style={{paddingTop:'30px'}}>
+    <StaticImage
+      src="../images/Personal Image.svg"
+      width={300}
+      height={350}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem`}}
+    />
+    </div>
+
+      
+
+    <div style={{paddingTop:'30px', paddingLeft:'30px'}}>
+    <StaticImage
+      src="../images/KingZ.jpg"
+      width={300}
+      height={350}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem`}}
+    />
+    </div>
+
+
+    </div>
+
+
+    <h1 style={{paddingTop:'100px', paddingBottom:'5px'}}>About Me</h1>
+
+      <div>
+          <h4 style={{fontWeight:'normal', paddingTop:'20px'}}>I’m a <span style={{ color:'blue', fontWeight:'bold'}}>Computer Science </span> 
+          and <span style={{color:'red', fontWeight:'bold'}}>Business 
+          </span> student at  <span style={{color:'purple', fontWeight:'bold'}}> Western University</span>. </h4>
+
+          <h4 style={{fontWeight:'normal', paddingTop:'50px'}}>I love <span style={{ color:'green', fontWeight:'bold'}}>Entrepreneurship</span>, 
+          <span style={{color:'rgb(255, 51, 153)', fontWeight:'bold'}}> consulting
+          </span>, and <span style={{color:'rgb(0, 204, 255)', fontWeight:'bold'}}> student politics</span>. </h4>
+
+
+          <h4 style={{fontWeight:'normal', paddingTop:'50px'}}>I’m originally from <span style={{ color:'rgb(0, 204, 0)', fontWeight:'bold'}}>Oakville </span>
+           and I love <span style={{color:'rgb(255, 153, 51)', fontWeight:'bold'}}>basketball</span>. </h4>
+
+
+      </div>
+
+
+      <div style={{paddingTop:'30px', paddingBottom:'30px'}}>
+          <StaticImage
+          src="../images/vector3.svg"
+          width={700}
+          height={5}
+          quality={95}
+          formats={["AUTO", "WEBP", "AVIF"]}
+          alt="A Gatsby astronaut"
+          style={{ marginBottom: `1.45rem`}}
+          />
+      </div>
+
+
+
+
+      <div  style={{display:'flex'}}>
+      
+      <div style={{paddingRight:'20px', paddingLeft:'20px'}}>
+      <StaticImage
+          src="../images/western_gang.svg"
+          width={150}
+          height={170}
+          quality={95}
+          formats={["AUTO", "WEBP", "AVIF"]}
+          alt="A Gatsby astronaut"
+          />
+      </div>
+
+      <div style={{paddingRight:'20px', paddingLeft:'20px'}}>
+      <StaticImage
+          src="../images/entre bulb.svg"
+          width={155}
+          height={150}
+          quality={95}
+          formats={["AUTO", "WEBP", "AVIF"]}
+          alt="A Gatsby astronaut"
+          />
+        </div>
+
+      <div style={{paddingRight:'20px', paddingLeft:'20px'}}>
+      <StaticImage
+          src="../images/lebron.svg"
+          width={155}
+          height={170}
+          quality={95}
+          formats={["AUTO", "WEBP", "AVIF"]}
+          alt="A Gatsby astronaut"
+          />
+      </div>
+
+      </div>
+
+
+
+
+
+
   </Layout>
 )
 
